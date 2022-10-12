add2ac4b65ffd7269914d825002fb1e8e084c6a3
sharkbound <laptopblaster@gmail.com>
2022-10-12 16:14:03 -0500

    observer stuffs

--- patterns/observer/run_observer.ts
+++ patterns/observer/run_observer.ts
@@ -28,10 +28,9 @@ type Item = { name: string, amount: number }
 interface BaseEvent {
 }
 
-interface ItemEvent extends BaseEvent {
+interface ItemEvent<T extends Item> extends BaseEvent {
     type: string
-    name: string
-    amount: number
+    item: Item
 }
 
 
@@ -40,34 +39,62 @@ interface Database<T extends Item> {
     remove: (T) => void
 }
 
+type EventTypeOptions = 'add' | 'remove' | 'delete' | 'any'
+
 class ItemDatabase<T extends Item> implements Database<T> {
-    private items: T[]
-    private event: Observer<ItemEvent>
+    private items: { [key: string]: T }
+    private event: Observer<ItemEvent<T>>
 
     constructor() {
-        this.items = []
-        this.event = createObserver<ItemEvent>()
+        this.items = {}
+        this.event = createObserver<ItemEvent<T>>()
     }
 
     add(item: T): void {
-        this.items.push(item)
+        if (this.items.hasOwnProperty(item.name)) {
+            this.items[item.name].amount += item.amount
+        } else {
+            this.items[item.name] = item
+        }
+
         this.event.publish({
             type: 'add',
-            name: item.name,
-            amount: item.amount
+            item: item
         })
     }
 
     remove(item: T): void {
-        // todo
-        this.event.publish({type: 'remove', name: item.name, amount: item.amount})
+        if (!this.items.hasOwnProperty(item.name)) {
+            return
+        }
+
+        let currentItem = this.items[item.name]
+        currentItem.amount -= item.amount
+
+        if (currentItem.amount <= 0) {
+            delete this.items[item.name]
+            currentItem.amount = 0
+
+            this.event.publish({
+                    type: 'delete',
+                    item: currentItem
+                }
+            )
+        } else {
+            this.event.publish({type: 'remove', item: item})
+        }
     }
 
-    listen(listener: (ev: ItemEvent) => void): () => void {
-        return this.event.subscribe(listener)
+    listen(eventType: EventTypeOptions, listener: Listener<ItemEvent<Item>>): () => void {
+        return this.event.subscribe((e) => {
+            if (eventType == 'any' || e.type === eventType) {
+                listener(e)
+            }
+        })
     }
 }
 
 let db = new ItemDatabase<Item>()
-db.listen((e) => console.log(`item added: ${e.name}(${e.amount})`))
+db.listen('any', (e) => console.log(`event: ${e.type}`))
 db.add({name: 'milk', amount: 100})
+db.remove({name: 'milk', amount: 100})
